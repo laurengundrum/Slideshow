936e268bc50aee103a1ec370289cea06436d9945
laurengundrum <lauren.gundrum@gmail.com>
2012-06-16 11:09:09 -0400

    moved files over from Google Drive

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,6 +1,15 @@
+//To do: 
+//1. make "new show" reset all counters, clear the pictures out of the divs, take you back to search div
+//2. add show name to popup_box
+//3. sortable() 
+//4. Custom animation?
+
 $(function() {
 
-	//Flickr searc
+	//initialize dialog box
+	$("#dialogSearch").dialog({title: 'Search and Select Pictures  ', closeText: 'x'});
+
+	//Flickr search
 	$('#searchFlickrButton').click(searchFlickr);
 
 	//move Flickr images to Project Pictures
@@ -9,55 +18,89 @@ $(function() {
 		$("#listOfPhotos").append($(this));
 	});
 
+	var imagesAvailable = false;
 	//move images from Project Pictures to Create Show 
 	$("#projectPictures").on('click', 'li', function() {
-		$(this).addClass(".ui-state-highlight");
 		$(this).remove();
-		$("#createShow ul").append($(this));
-		createShowList = $("#createShow ul");
+		$("#showPictures").append($(this));
+		createShowList = $("#showPictures");
+		imagesAvailable = true;
 	});
 
 	//Button click events
-		$("#goButton").click(function() {
-			window.location = "selectpictures.html";
-		})
 
+	//Clicking on Create takes you to a new tab, where you can arrange photos and view your show
+	var dialogDisplayedBefore = false; 
 		$("#createShowButton").click(function() {
-			if(createShowDisplayed == false) {
+			$("#dialogSearch").dialog('close');
+			if (createShowDisplayed==false){
 				loadCreateShow();
+				switchSelectedClass($(this));
+				$('#viewButton').removeClass('hidden');
+				if (dialogDisplayedBefore == false) {
+					$( "#dialogCreate" ).dialog({title: 'Create Your Show   ', closeText: 'x'});
+					dialogDisplayedBefore = true;
+				}
 			}
+
+
 		});
 
+		//you can click back to Search and Select Photos if you want to add more
 		$("#selectButton").click(function() {
 			if(selectPicturesDisplayed == false) {
 				loadSelectPictures();
+				switchSelectedClass($(this));
 			}
+			if ($('#viewButton').hasClass('hidden')) {
+				return;
+			}
+			else {
+				$("#viewButton").addClass('hidden');
+			}
+			$("#dialogCreate").dialog('close');
 		});
 
-	//Button hover events
-		$(".blueButton").hover(function() {
+		//View Show pop-up box
+		$("#viewButton").click(function() {
+			if(imagesAvailable==false) {
+				alert('You need to choose some pictures first!');
+			}
+
+			else if(viewShowDisplayed == false && imagesAvailable == true) {
+				viewShow();
+			} 
+			$("#dialogCreate").dialog('close');
+		});
+
+		$("#viewButton").hover(function() {
 			$(this).addClass('active');
-		}, 
+		},
 			function() {
 				$(this).removeClass('active');
 			}
 		);
 
-	//View Show pop-up box
-		$("#viewButton").click(function() {
-			if(createShowDisplayed == true) {
-				viewShow();
-			} 
-			else {
-				alert("Choose some pictures in order to view your show :-)");
+		//New Show 
+		$('#newShow').click(function() {
+			dialogDisplayedBefore = false;
+			photoAlbums = [];
+			selectPicturesDisplayed = true;
+			createShowDisplayed = false;
+			viewShowDisplayed = false;
+			createShowList = [];
+			loadSelectPictures();
+			imagesAvailable = false;
+		})
+
+	/*Button hover events
+		$("#blueButton li").hover(function() {
+			$(this).addClass('active');
+		}, 
+			function() {
+				$(this).removeClass('active');
 			}
-		});
-
-		$('#popupBoxClose').click( function() {            
-            $('#popup_box').fadeOut("slow");
-            $('#ul').empty();
-        });
-
+		);*/
 
 });
 
@@ -76,7 +119,6 @@ function flickrResults(data)
 	var photos = data.photos.photo;
 	var photosLength = photos.length;
 
-
 	for (var i=0; i<photosLength; i++) {
 		var item = photos[i];
 		var src = "http://farm"+ item.farm +".static.flickr.com/"+ item.server +"/"+ item.id +"_"+ item.secret +"_m.jpg";
@@ -87,36 +129,42 @@ function flickrResults(data)
 }
 
 var photoAlbums = [];
-var createShowDisplayed = false;
 var selectPicturesDisplayed = true;
+var createShowDisplayed = false;
+var viewShowDisplayed = false;
+
 var loadCreateShow = function() {
 	createShowDisplayed = true;
 	selectPicturesDisplayed = false;
+	viewShowDisplayed = false;
 	photoAlbums = $("#photoAlbums");
-	$("#photoAlbums").detach();
-	$(".showContainer").prepend("<div class='grid_8 alpha box' id='createShow'>" + 
-						"<h2>Create Show</h2>" + 
-						"<p>Click photos to move them to your show, then drag to change the order.</p>" + 
-						"<ul id = 'showPictures'>" + "</ul>" + "<div id = 'viewButton'>" + "</div>" +
-					"</div>");
-
-	$("#createShow ul").sortable();
+	$("#photoAlbums").addClass('hidden');
+	$("#createShow").removeClass('hidden');
+	$("#showPictures").disableSelection();
 }
 
 var loadSelectPictures = function() {
-	createShowDisplayed = false;
 	selectPicturesDisplayed = true;
-	$("#createShow").detach();
-	$(".showContainer").prepend(photoAlbums);
+	createShowDisplayed = false;
+	viewShowDisplayed = false;
+	$("#createShow").addClass('hidden');
+	$('#photoAlbums').removeClass('hidden');
 }
 
 var viewShow = function() {
+	selectPicturesDisplayed = false;
+	createShowDisplayed = false;
+	viewShowDisplayed = true;
 	var toShow = createShowList.children().first();
+	$('#popup_box').prepend("<div id = 'popupTitle'>" + $('#projectName').val() + "</div>");
 	$('#popup_box').fadeIn("slow");
-	$("#ul").append(createShowList);
-	$("#ul li").addClass("hidden");
+	$("#popUpPics").append(createShowList);
+	$("#popUpPics li").addClass("hidden");
 	$("#playButton").click(function() {
 			callNextPicture(toShow);
+	});
+	$("#popupBoxClose").click(function() {
+		$('#popup_box').fadeOut("slow");
 	});
 }
 
@@ -125,4 +173,9 @@ var callNextPicture = function(picture) {
 	picture.fadeIn(800).delay(delay).fadeOut(800, function() {
 		callNextPicture(picture.next());
 	});
+}
+
+function switchSelectedClass(clickedTab) {
+	clickedTab.siblings('.selected').removeClass('selected').addClass('notSelected');
+	clickedTab.removeClass('notSelected').addClass('selected');
 }
