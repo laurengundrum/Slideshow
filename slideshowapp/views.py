ad142ef6788f021a68dc8d64aa3b1362c0ed28e2
Andrew Brown <andrew@andrewcbrown.com>
2012-06-16 12:25:53 -0400

    removed csrf protection, which was a copy-paste error from another project

--- slideshowapp/views.py
+++ slideshowapp/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponse, Http404
 
-@ensure_csrf_cookie
 def index(request):
 	return render_to_response('selectpictures.html')
