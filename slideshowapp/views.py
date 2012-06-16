363d60f94ef63a5c8408ba84fd50ee14c171e708
Andrew Brown <andrew@andrewcbrown.com>
2012-06-16 12:39:21 -0400

    updated template to use STATIC_URL context

--- slideshowapp/views.py
+++ slideshowapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponse, Http404
+from django.template import RequestContext
 
 def index(request):
-	return render_to_response('selectpictures.html')
+	return render_to_response('selectpictures.html', [],
+                              context_instance=RequestContext(request))
